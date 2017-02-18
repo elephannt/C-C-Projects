3ae14d9d3682228cc1f89cb9d49697c8f6a28414
elephannt <lopezvalmarco@gmail.com>
2017-02-17 19:39:04 -0800

    Hello World!

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Marco on 17/02/2017.
+//
+
+// A hello world program in C++
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World this is my first program on C++!";
+    return 0;
+}
