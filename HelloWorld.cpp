bdcbfedad679657f4b4734357d3bfbfe3215c104
elephannt <lopezvalmarco@gmail.com>
2017-02-17 20:07:58 -0800

    Hello World!

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,11 +4,21 @@
 
 // A hello world program in C++
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    cout << "Hello World this is my first program on C++!";
+    double firstNumber, secondNumber, productOfTwoNumbers;
+    cout << "Enter two numbers: ";
+
+    // Stores two floating point numbers in variable firstNumber and secondNumber respectively
+    cin >> firstNumber >> secondNumber;
+
+    // Performs multiplication and stores the result in variable productOfTwoNumbers
+    productOfTwoNumbers = firstNumber * secondNumber;
+
+    cout << "Product = " << productOfTwoNumbers;
+
     return 0;
 }
